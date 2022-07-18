9b80280eabb196c290b1ef5d8d5c8ab0068a1d01
Giovanni Lopes <giovanniclopes@gmail.com>
2022-07-18 01:20:36 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,12 @@
+import Logo from '/src/assets/logo.png'
+
 export function Footer() {
     return (
         <footer className="flex flex-col items-center justify-center mt-10">
             <div>
                 <img 
                 className="w-36 h-40"
-                src=".\src\assets\logo.png" alt="" />
+                src={Logo} alt="" />
                 <span></span>
             </div>
         </footer>
