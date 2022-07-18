f36ba6b15f359eca5010b1584734329f31ae4d20
Giovanni Lopes <giovanniclopes@gmail.com>
2022-07-18 00:39:03 -0300

    all texts and footer added

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+export function Footer() {
+    return (
+        <footer className="flex flex-col items-center justify-center mt-10">
+            <div>
+                <img 
+                className="w-36 h-40"
+                src="src\assets\logo.png" alt="" />
+                <span></span>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
