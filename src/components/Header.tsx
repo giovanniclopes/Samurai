4e2ad281c707fee7d9b11ec594be64fffacb9166
Giovanni Lopes <giovanniclopes@gmail.com>
2022-07-18 02:07:24 -0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,13 +5,13 @@ export function Header() {
     return (
         <header className="fixed bg-cpBlack w-full h-56 flex flex-auto items-center justify-center">
             <img 
-            className="w-48 h-48 select-none sm:w-24 h-24"
+            className="w-48 h-48 select-none sm:w-24 h-auto"
             src={Logo} alt="" draggable='false' />
             <img
-            className="w-auto h-72 select-none sm:w-60 h-44"
+            className="w-auto h-72 select-none sm:w-52 h-auto"
             src={Samurai} alt="" draggable='false'/>
             <img
-            className="w-48 h-48 select-none sm:w-24 h-24"
+            className="w-48 h-48 select-none sm:w-24 h-auto"
             src={Logo} alt="" draggable='false' />
         </header>
     )
