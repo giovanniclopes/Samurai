5b5c87099caec0f35bd1dd0a12edf6e3d55f420f
Giovanni Lopes <giovanniclopes@gmail.com>
2022-07-18 01:25:27 -0300

    image error fixed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,18 @@
+import Logo from '/src/assets/logo.png'
+import Samurai from '/src/assets/samurai.png'
+
 export function Header() {
     return (
         <header className="fixed bg-cpBlack w-full h-56 flex flex-auto items-center justify-center">
             <img 
             className="w-48 h-48 select-none"
-            src="src/assets/logo.png" alt="" draggable='false' />
+            src={Logo} alt="" draggable='false' />
             <img
             className="w-auto h-72 select-none"
-            src="src/assets/samurai.png" alt="" draggable='false'/>
+            src={Samurai} alt="" draggable='false'/>
             <img
             className="w-48 h-48 select-none"
-            src="src/assets/logo.png" alt="" draggable='false' />
+            src={Logo} alt="" draggable='false' />
         </header>
     )
 }
